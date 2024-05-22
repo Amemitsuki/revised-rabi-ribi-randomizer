1f5101ce84dee04b8eb1eea10143d6a22ccb3e49
Amemitsuki <168346685+Amemitsuki@users.noreply.github.com>
2024-05-22 17:14:43 +0800

    Add workflow and scripts

---
+++ run_extreme.sh
@@ -0,0 +1,20 @@
+echo "## OBS_EXT"
+mkdir -p generated_extreme
+python3 ./randomizer.py \
+    -config-file config_extreme.txt \
+    -output-dir generated_extreme \
+    --shuffle-music \
+    --shuffle-backgrounds \
+    --shuffle-map-transitions \
+    --shuffle-gift-items \
+    -constraint-changes 20 \
+    --no-difficult-backgrounds \
+    --hyper-attack-mode \
+    --open-mode \
+    --egg-goals \
+    -extra-eggs 2 \
+    -num-hard-to-reach 5 \
+    -min-difficulty 22 \
+    -min-chain-length 8 \
+    --debug-visualize \
+    -max-attempts 2147483647
