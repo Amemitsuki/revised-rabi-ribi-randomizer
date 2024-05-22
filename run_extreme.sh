68edb4262ac1523cbd72ac06b14f453b8de897ab
Amemitsuki <168346685+Amemitsuki@users.noreply.github.com>
2024-05-22 17:14:43 +0800

    Add workflow and scripts

---
+++ run_extreme.sh
@@ -0,0 +1,16 @@
+python3 ./randomizer.py \
+    -config-file config_extreme.txt \
+    --shuffle-music \
+    --shuffle-backgrounds \
+    --shuffle-map-transitions \
+    --shuffle-gift-items \
+    -constraint-changes 20 \
+    --no-difficult-backgrounds \
+    --hyper-attack-mode \
+    --open-mode \
+    --egg-goals \
+    -extra-eggs 2 \
+    -num-hard-to-reach 5 \
+    -min-difficulty 24 \
+    -min-chain-length 8 \
+    -max-attempts 2147483647
